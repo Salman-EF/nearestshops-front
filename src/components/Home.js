28776ad09ef88c25f402f836962c55d952c88733
SALMAN <elfadili.salman@gmail.com>
2019-01-02 00:24:58 +0100

    Finished Home component as parent of Shops and PreferredShops components

---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import '../App.css';
+import {Container} from "mdbreact";
+
+import Navbar from './shops/Navbar'
+import Shops from './shops/all/Shops'
+import PreferredShops from './shops/preferred/PreferredShops'
+
+class Home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      redirect: this.props.redirect
+    }
+  }
+  componentDidMount() {
+    let token = localStorage.getItem('ACCESS_TOKEN')
+    if(!token) {
+      this.props.history.push("/login")
+    }
+    if(this.state.redirect==='shops') {
+      this.props.history.push("/shops")
+    }
+  }
+
+  render() {
+    let redirect = this.state.redirect
+    return (
+      <Container>
+        <Navbar />
+        {
+          redirect==='preferredShops' ? (
+            <PreferredShops />
+          ) : (
+            <Shops />
+          )
+        }
+      </Container>
+    )
+  }
+}
+
+export default withRouter(Home);
