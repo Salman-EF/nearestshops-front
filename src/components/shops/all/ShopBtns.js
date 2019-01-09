5c09d56e9aea5d09504eb341c6752e352ae6ad52
SALMAN <elfadili.salman@gmail.com>
2019-01-09 11:42:36 +0100

    Build dislikeShop functionnality with refreshing the shops list with api response as user dislikedShops

--- src/components/shops/all/ShopBtns.js
+++ src/components/shops/all/ShopBtns.js
@@ -24,10 +24,29 @@ class ShopBtns extends Component {
             origin.props.updateShopsList(shops)
           })
     }
+    dislikeShop = () =>{
+        console.log('Shops disliked: '+this.state.shop.id)
+        let token = localStorage.getItem('ACCESS_TOKEN'), origin = this
+        fetch('http://localhost:8080/api/shops/disliked',{
+          method: "POST",
+          headers: { "Authorization": token,"Content-Type": "application/json" },
+          body: JSON.stringify(this.state.shop)
+        }).then(response => response.json())
+          .then(data => {
+            let shops = origin.state.shops
+                data.map(dislikeShop => {
+                    shops = shops.filter(shop => {
+                        return dislikeShop !== shop.id
+                    })
+                    return null
+                })
+            origin.props.updateShopsList(shops)
+          })
+    }
     render() {
         return (
             <MDBRow className="justify-content-center">
-                <MDBBtn color="danger">Dislike</MDBBtn>
+                <MDBBtn color="danger" onClick={this.dislikeShop}>Dislike</MDBBtn>
                 <MDBBtn color="success" onClick={this.likeShop}>Like</MDBBtn>
             </MDBRow>
         )
