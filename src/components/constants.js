f9f09309e9acc0546ce1b2fad0b496e705c80392
SALMAN <elfadili.salman@gmail.com>
2021-12-28 21:26:49 +0100

    change API_BASE_URL

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,7 @@
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
 
-export const API_BASE_URL = 'https://nearby-shops-api.herokuapp.com/api';
-export const LOGIN = 'https://nearby-shops-api.herokuapp.com/login';
+export const API_BASE_URL = 'https://nearestshops-api.herokuapp.com/api';
+export const LOGIN = API_BASE_URL+'/login';
 export const REGISTER = API_BASE_URL+'/register';
 export const LOGOUT = API_BASE_URL+'/logout';
 export const USER_ME = API_BASE_URL+'/users/me';
